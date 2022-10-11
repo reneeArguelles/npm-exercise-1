617dedf40b8a68b16acc8b66a38101aa450be50b
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-11 09:33:52 +0800

    Apply prettier to js files

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const qrcode = require('qrcode-terminal');
 
-console.log(qrcode.generate("WIFI:T:WPA;S:SOCIALDESK - 9th Floor;P:SocialD3$K09F;;"));
+console.log(
+  qrcode.generate('WIFI:T:WPA;S:SOCIALDESK - 9th Floor;P:SocialD3$K09F;;')
+);
 
-console.log(qrcode.generate(`BEGIN:VCARD
+console.log(
+  qrcode.generate(`BEGIN:VCARD
 N:Arguelles;Mary Renee 
 EMAIL:mary.arguelles@siteminder.com
 TEL;WORK;VOICE:09171487132
-END:VCARD`));
+END:VCARD`)
+);
